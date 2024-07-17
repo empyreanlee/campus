27237865052fcb5d30ca11b4039096e74335db12
Jeremy Gitau <j.leegitau@gmail.com>
2024-07-17 23:09:06 +0300

    semester

--- src/main/java/org/assign/campus/StudentController.java
+++ src/main/java/org/assign/campus/StudentController.java
@@ -1,26 +1,47 @@
 package org.assign.campus;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.layout.GridPane;
 
-public class StudentController {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class StudentController implements Initializable {
     public Button register;
     public GridPane gridPane;
-    public ComboBox<String> semester1;
-    public ComboBox<String> semester2;
+
+    @FXML
+    private ListView<String> listView1;
+    @FXML
+    private ListView<String> listView2;
+
+    private final ArrayList<String> selectedCourses = new ArrayList<>();
+
 
     @FXML
     protected void onRegisterButtonClick() {
+        List<String> selectedItems = listView1.getSelectionModel().getSelectedItems();
+        selectedCourses.clear();
+        selectedCourses.addAll(selectedItems);
+        for (String course : selectedCourses){
+            System.out.println(course);
+        }
 
     }
-
-    public void initialize() {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
         String[] courses1 ={"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
         String[] courses2 ={"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
-        semester1.getItems().addAll(courses1);
-        semester2.getItems().addAll(courses2);
+
+        listView1.getItems().addAll(courses1);
+        //listView2.getItems().addAll(courses2);
+        listView1.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 
     }
 }
